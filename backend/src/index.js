2c0ccbd91922beb85c5e06d333bce66e0709a9cd
007Gowtham <gowthams200521@gmail.com>
2025-11-01 17:50:43 +0530

    database connection is ready to use

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import app from "./app.js";
 import logger from "./logger/winston.logger.js";
-
+import { connectDB } from "./db/index.js";
 const PORT = process.env.PORT || 3000;
+
+await connectDB();
+
 
 app.listen(PORT, () => {
   logger.info(`🚀 Server is running on http://localhost:${PORT}`);
